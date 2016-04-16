62f8cb663139a5e17cfd3c84353965e443462daa
Tyler Lima <tpl8254@rit.edu>
2016-04-16 13:42:51 -0400

    Adding the Sudoku Puzzle Solver to GitHub

---
+++ src/SudokuPTUI.java
@@ -0,0 +1,9 @@
+/**
+ * The Sudoku plain text user interface
+ * Created by tyler on 4/16/2016.
+ */
+public class SudokuPTUI {
+    public static void main(String[] args) {
+        System.out.println("Adding Project to Git");
+    }
+}
