e31b726c5468c6d33177ab777a6a4a4a576ad93b
mjschuetze102 <pieisawesome102@gmail.com>
2016-04-16 21:51:23 -0400

    Added more comments

--- src/SudokuPTUI.java
+++ src/SudokuPTUI.java
@@ -20,8 +20,10 @@ public class SudokuPTUI {
     public SudokuPTUI(String filename) throws FileNotFoundException {
         Scanner in= new Scanner(new File(filename));
 
+        // Create a new 9x9 grid to be filled in
         puzzleGrid= new char[9][9];
 
+        // Fill out the puzzle grid with input from a file
         for(int row= 0; row < puzzleGrid.length; row++){
             for(int col= 0; col < puzzleGrid[0].length; col++){
                 puzzleGrid[row][col]= in.next().charAt(0);
@@ -34,6 +36,7 @@ public class SudokuPTUI {
      * @param parent the parent SudokuPTUI layout
      */
     public SudokuPTUI(SudokuPTUI parent){
+        // Copy over the data in the parent SudokuPTUI
         System.arraycopy(parent.puzzleGrid, 0, this.puzzleGrid, 0, parent.puzzleGrid.length);
     }
 
@@ -43,23 +46,30 @@ public class SudokuPTUI {
      */
     @Override
     public String toString(){
+        // The string representing the puzzle
         String grid= "";
+        // The divide that appears on both sides of the middle 3x3 grids
         String horizLine= "-------|-------|-------\n";
 
         for(int row= 0; row < puzzleGrid.length; row++){
+            // Add a horizontal line after the 3rd and 6th row to separate the 3x3 grids
             if(row != 0 && row % 3 == 0)
                 grid+= horizLine;
 
+            // Add a space to the front of each line
             grid+= " ";
 
             for(int col= 0; col < puzzleGrid[0].length; col++){
+                // Add a space after each vertical divide between 3x3 grids
                 if(col != 0 && col % 3 == 0)
                     grid+= "| ";
 
+                // Copy over the character in each cell and add a space after it
                 grid+= puzzleGrid[row][col]+ " ";
 
             }
 
+            // Add a newline character after each line
             grid+= "\n";
         }
 
