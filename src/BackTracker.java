d98e3fdafe73058ac7496123323447897c76ecab
mjschuetze102 <pieisawesome102@gmail.com>
2016-04-22 23:14:40 -0400

    Finished BackTracker

--- src/BackTracker.java
+++ src/BackTracker.java
@@ -1,12 +1,51 @@
+import java.io.FileNotFoundException;
+
 /**
- * @author Tyler 4/22/2016.
- * @author Michael 4/22/2016
+ * The backtracking algorithm for solving the puzzle
+ * @author Tyler on 4/22/2016
+ * @author Michael on 4/22/2016.
  */
-
-import java.util.ArrayList;
-import java.util.HashSet;
-
-
 public class BackTracker {
-    public void
+
+    /**
+     * Constructor for a new BackTracker object
+     */
+    public BackTracker(){}
+
+    /**
+     * Finds all the outcomes from the current grid
+     * @param grid SudokuPTUI object to get the next move from
+     */
+    public SudokuPTUI solvePuzzle(SudokuPTUI grid){
+        // If the Sudoku puzzle is solved return the grid
+        if(grid.isGoal()){
+            return grid;
+        }else{ // If the puzzle has yet to be solved
+            for(SudokuPTUI newGrid : grid.getSuccessors()){
+                // If the grid does not break the rules of Sudoku
+                if(grid.validate()){
+                    SudokuPTUI solution= solvePuzzle(newGrid);
+                    // If a solution was found down this recursive path
+                    if(solution != null)
+                        return solution;
+                }
+            }
+
+        }
+
+        // If a solution was not found
+        return null;
+    }
+
+    /**
+     * Main function for the file
+     * Creates a new grid and finds all of its outcomes
+     * @param args program arguments defined by the user
+     */
+    public static void main(String[] args) throws FileNotFoundException{
+        SudokuPTUI grid= new SudokuPTUI(args[0]);
+        System.out.println(grid);
+        BackTracker backtracker= new BackTracker();
+        System.out.println(backtracker.solvePuzzle(grid));
+    }
 }
