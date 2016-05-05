a72bf9a8d01626084bfaa7766e03941c29ee35fa
Tyler Lima <tpl8254@rit.edu>
2016-05-04 23:32:11 -0400

    GUI class created and SudokuPTUI class updated to implement Observer

---
+++ src/SudokuGraphicalVC.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+
+/**
+ * Created by tyler on 5/4/2016.
+ *
+ */
+public class SudokuGraphicalVC extends Application implements Observer {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        GridPane mainPane = new GridPane();
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
